baa11fd45314727cfa5b3b8ddc3f68856cff97d0
Stephen Trinh <s6trinh@ucsd.edu>
2016-02-06 16:06:43 -0800

    finished some basic cards. they're json compatible

--- routes/allchores.js
+++ routes/allchores.js
@@ -1,5 +1,5 @@
+var card_data = require('../card-data.json');
+
 exports.viewProject = function(req, res){
-  res.render('allchores', {
-    'name' : "fubar"
-  });
+  res.render('allchores', card_data);
 };
