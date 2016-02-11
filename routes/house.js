b0c3f1f9fe44aa4f3663bc03a1762e7811abadc7
Alex Tang <atang1994@gmail.com>
2016-02-11 12:36:52 -0800

    fixed a typo

--- routes/house.js
+++ routes/house.js
@@ -7,13 +7,13 @@
 var houses = require('../data/houses.json');
 
 exports.create = function(req, res) {
-	res.cookie('house', house );
+	res.cookie('house', houses );
 	res.cookie('username', 'Bob');
 	return res.redirect('/mychores');
 };
 
 exports.join = function(req, res) {
-	res.cookie('house', house );
+	res.cookie('house', houses );
 	res.cookie('username', 'Sally');
 	return res.redirect('/mychores');
 };
