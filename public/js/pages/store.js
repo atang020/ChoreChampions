2e3f9b282ed67e34b0cd5118e6207f6c9941e2e9
Dexter Friedman <drfriedm@ucsd.edu>
2016-02-11 21:31:20 -0800

    Faking the store logic

--- public/js/pages/store.js
+++ public/js/pages/store.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var GOLD = 1337;
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
   initializePage();
@@ -9,13 +11,23 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+
+  // TODO: Remove the Oz
   console.log("Javascript connected!");
+  $(".store-item").click(function(){
+  	var item = $(this).parent().find('h1.text-center').text();
+  	var price = Number.parseInt( $(this).parent().find('input.price-hack').val() );
+  	// console.log('item: ' + item);
+  	// console.log('price: ' + price)
+  	var yes = confirm("Buy this item for " + price + " gold?");
+  	if ( yes == true ) {
+  		if ( GOLD - price < 0 ) {
+  			alert("Sorry, you don't have enough gold yet.");
+  		}
+  		else {
+  			alert("Purchased 1 x " + item + '!' );
+  			GOLD -= price;
+  		}
+  	}
+  });
 }
-
-$(".fubar").click(function(){
-  alert("Are you sure you want to buy?");
-});
-
-$(".saveChanges").click(function(){
-  alert("Settings Changed!");
-});
